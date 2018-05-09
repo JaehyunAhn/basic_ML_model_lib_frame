f189effa34337b213da5f37e6b5397b329325ae5
sogo <sogo@buzzni.com>
2018-05-09 18:05:24 +0900

    Initial commit

---
+++ newmodel.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+def _private_method():
+    '''
+    Adding _ (underscore) means that you use this method only within
+        this scope(in this case, <_private_method> function only allowed
+        to use in 'newmodel.py' file. If this kind of convention used in Class,
+        then that function only allow to use in a private way(within a class only).
+    '''
+    return None
+
---
+++ usage.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+'''
+practical usage example of your new model.
+'''
+
+import newmodel
+
+if __name__ == '__main__':
+    pass
